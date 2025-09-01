a2ab774a4aab556829304d47e7d8cb37552e6854
unknown <dy110703@gmail.com>
2025-09-01 11:51:04 +0700

    Penyesuaian Landing Page di Figma 50%

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './pages/App.tsx'
+import App from './pages/LandingPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
